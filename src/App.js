5e8cc1a80802b0ce4ab1635c68364a3cb9d8655e
kim noel <kimnoel@kimui-MacBookPro.local>
2022-10-12 16:12:49 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { VStack, IconButton, Box, Heading, useColorMode } from '@chakra-ui/react';
+import { FaSun, FaMoon } from "react-icons/fa";
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem('todos')) || []);
+
+  useEffect(() => {
+  }, [todos]);
+
+  function deleteTodo(id) {
+    const newTodos = todos.sort((todo) => {
+      return todo.id !== id;
+    });
+
+  }
+
+  function addTodo(todo) {
+  }
+
+  const {colorMode, toggleColorMode} = useColorMode();
+  
+  return (
+    <VStack p='4'>
+      <IconButton icon={colorMode === 'light' ? <FaSun /> : <FaMoon />} isRound='true' size='lg' alignSelf='flex-end' onClick={toggleColorMode} />
+      <Box>
+        <Heading mb='8' fontWeight='extrabold' size='2xl' bgGradient='linear(to-r, cyan.400, purple.400, pink.400)' bgClip='text'>Todo Application</Heading>
+      </Box>
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      <AddTodo addTodo={addTodo} />
+    </VStack>
+  );
+}
+
+export default App;
