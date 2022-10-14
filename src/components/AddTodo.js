48de756e128ef760d8357461a80fe9ee9fb744c9
wookyung2 <gyong1540@naver.com>
2022-10-14 14:06:20 +0900

    Input 입력 활성화

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import { HStack, Input, Button, useToast } from '@chakra-ui/react';
-import { nanoid } from 'nanoid';
+import React, { useState } from "react";
+import { HStack, Input, Button, useToast } from "@chakra-ui/react";
+import { nanoid } from "nanoid";
 
-const AddTodo = ({addTodo}) => {
-  const [content, setContent] = useState('');
+const AddTodo = ({ addTodo }) => {
+  const [content, setContent] = useState("");
   const toast = useToast();
-  
+
   function handleSubmit(e) {
     e.preventDefault();
 
-    if(!content) {
+    if (!content) {
       toast({
-        title: 'No content',
-        status: 'error',
+        title: "No content",
+        status: "error",
         duration: 2000,
-        isClosable: true
+        isClosable: true,
       });
 
       return;
@@ -22,20 +22,30 @@ const AddTodo = ({addTodo}) => {
 
     const todo = {
       id: nanoid(),
-      body: content
+      body: content,
     };
 
     addTodo(todo);
+    setContent("");
   }
-  
+
   return (
     <form onSubmit={handleSubmit}>
-      <HStack mt='8'>
-        <Input variant='filled' placeholder='Learning React' value={content} onChange={() => {}} />
-        <Button type='submit' colorScheme='cyan' px='8'>Add Todo</Button>
+      <HStack mt="8">
+        <Input
+          variant="filled"
+          placeholder="Learning React"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+        <Button type="submit" colorScheme="cyan" px="8">
+          Add Todo
+        </Button>
       </HStack>
     </form>
   );
-}
+};
 
 export default AddTodo;
